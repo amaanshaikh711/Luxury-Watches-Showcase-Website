59fd10440147360e88ac36610bab4824044716cc
Sajeed <sajeed@Sajeeds-Laptop.local>
2024-09-24 22:24:43 +0530

    updated mobile menu and added button view more

--- js/script.js
+++ js/script.js
@@ -10,17 +10,25 @@ fetch('include/header.html')
     .then(response => response.text()) 
     .then(html => { 
         document.getElementById('header').innerHTML = html; 
+        menuToggle();
     }) 
     .catch(error => console.error('Error loading HTML:', error)); 
 
 
+/***** SHOW MOBILE MENU  ******/  
+function menuToggle(params) {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+    console.log(navLinks);
+    console.log(navLinks.classList)
 
-const menuToggle = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
+    menuToggle.addEventListener('click', () => {
+        console.log(navLinks.classList);
+        
+        navLinks.classList.toggle('active');
+    });
+}
 
-// menuToggle.addEventListener('click', () => {
-//     navLinks.classList.toggle('active');
-// });
 
 // $(document).ready(function(){
 //     $('.buy-now-btn').hover(function(){
